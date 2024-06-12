b198edc869d338c88b9e0f37641414659184b1ea
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-12 13:30:49 -0600

    feat(Sidebar): implement the component container layout

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,5 +1,9 @@
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <section className="sticky left-0 top-0 flex h-screen w-fit fle-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6"></div>
+    </section>
+  );
 };
 
 export default Sidebar;
