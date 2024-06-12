f879712d23c6e27b75386612cb7a7bc4cf531cb5
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-12 16:25:17 -0600

    feat(MobileNav): implement the shadcn Sheet component initial layout

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,5 +1,29 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
 };
 
 export default MobileNav;
