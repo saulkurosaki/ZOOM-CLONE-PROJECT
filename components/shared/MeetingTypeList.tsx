1fe3c8bee04a496e3b227655325334a227937c40
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-13 12:02:59 -0600

    feat(MeetingTypeList): implement the add new meeting icon

--- components/shared/MeetingTypeList.tsx
+++ components/shared/MeetingTypeList.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Image from "next/image";
+
 const MeetingTypeList = () => {
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
@@ -7,7 +9,14 @@ const MeetingTypeList = () => {
         className="bg-orange-1 px-4 p-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer"
         onClick={() => {}}
       >
-        Box 1
+        <div className="flex-center glassmorphism size-12 rounded-[10px]">
+          <Image
+            src="/icons/add-meeting.svg"
+            alt="meeting icon"
+            width={27}
+            height={27}
+          />
+        </div>
       </div>
     </section>
   );
