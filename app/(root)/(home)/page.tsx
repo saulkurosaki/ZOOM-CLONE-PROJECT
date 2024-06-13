2e83efcf9fbf112bef8a22ac7d5491fc1b384689
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-13 11:25:29 -0600

    feat(Home): implement the hero banner container layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Home</h1>
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover"></div>
     </section>
   );
 };
