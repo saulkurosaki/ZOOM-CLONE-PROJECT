a1190d5a3c57753825fa151cc9e5986d0baa8292
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-13 12:19:14 -0600

    feat(HomeCard): implement the component dynamic color

--- components/shared/HomeCard.tsx
+++ components/shared/HomeCard.tsx
@@ -1,9 +1,27 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
-const HomeCard = () => {
+interface HomeCardProps {
+  className: string;
+  image: string;
+  title: string;
+  description: string;
+  handleClick: () => void;
+}
+
+const HomeCard = ({
+  className,
+  image,
+  title,
+  description,
+  handleClick,
+}: HomeCardProps) => {
   return (
     <div
-      className="bg-orange-1 px-4 p-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer"
+      className={cn(
+        "px-4 p-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer",
+        className
+      )}
       onClick={() => {}}
     >
       <div className="flex-center glassmorphism size-12 rounded-[10px]">
