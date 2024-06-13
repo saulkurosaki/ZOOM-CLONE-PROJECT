74f30b32fda84bd74496e38266db4308514ef02a
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-13 12:37:53 -0600

    feat(MeetingModal): implement the dialog image display if there is one

--- components/shared/MeetingModal.tsx
+++ components/shared/MeetingModal.tsx
@@ -8,6 +8,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import Image from "next/image";
 
 interface MeetingModalProps {
   isOpen: boolean;
@@ -34,7 +35,15 @@ const MeetingModal = ({
 }: MeetingModalProps) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white"></DialogContent>
+      <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+        <div className="flex flex-col gap-6">
+          {image && (
+            <div>
+              <Image src={image} alt="image" width={72} height={72} />
+            </div>
+          )}
+        </div>
+      </DialogContent>
     </Dialog>
   );
 };
