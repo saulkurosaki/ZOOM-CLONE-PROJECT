5bc9dc8104f1acc942e1cfe80bb3a75a2d8a2329
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-13 12:39:36 -0600

    feat(MeetingModal): implement the dialog title display

--- components/shared/MeetingModal.tsx
+++ components/shared/MeetingModal.tsx
@@ -9,6 +9,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 interface MeetingModalProps {
   isOpen: boolean;
@@ -38,10 +39,14 @@ const MeetingModal = ({
       <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
         <div className="flex flex-col gap-6">
           {image && (
-            <div>
+            <div className="flex justify-center">
               <Image src={image} alt="image" width={72} height={72} />
             </div>
           )}
+
+          <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>
+            {title}
+          </h1>
         </div>
       </DialogContent>
     </Dialog>
