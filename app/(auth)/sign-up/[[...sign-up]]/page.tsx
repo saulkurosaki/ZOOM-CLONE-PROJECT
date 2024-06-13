f284a8e5587cedfe389e7eac3f3e5ec26686d116
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-13 11:08:03 -0600

    style(RootLayout): implement some custom styles to ClerkProvider component

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 const SignUpPage = () => {
   return (
-    <main>
+    <main className="flex h-screen w-full items-center justify-center">
       <SignUp />
     </main>
   );
