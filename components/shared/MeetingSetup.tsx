3414f810e3a00113e36aa4357aa6093675cb426f
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-14 10:56:01 -0600

    feat(MeetingSetup): implement the camera and microphone enable useEffect along as the camera video display feature

--- components/shared/MeetingSetup.tsx
+++ components/shared/MeetingSetup.tsx
@@ -1,5 +1,32 @@
+"use client";
+
+import { VideoPreview, useCall } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
+
 const MeetingSetup = () => {
-  return <div>MeetingSetup</div>;
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
+  const call = useCall();
+
+  if (!call) {
+    throw new Error("useCall must be used within StreamCall component");
+  }
+
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+  }, [isMicCamToggledOn, call?.camera, call?.microphone]);
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+      <h1 className="text-2xl font-bold">Setup</h1>
+      <VideoPreview />
+    </div>
+  );
 };
 
 export default MeetingSetup;
