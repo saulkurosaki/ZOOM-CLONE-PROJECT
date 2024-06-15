9dd8e5c35958ebdfe1ff10f2b780d0f62347e736
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-14 23:28:53 -0600

    feat(MeetingRoom): implement the participants different layout feature

--- components/shared/MeetingRoom.tsx
+++ components/shared/MeetingRoom.tsx
@@ -63,13 +63,20 @@ const MeetingRoom = () => {
               <LayoutList size={20} className="text-white" />
             </DropdownMenuTrigger>
           </div>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+            {["Grid", "Speaker-Left", "Speaker-Right"].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem
+                  className="cursor-pointer"
+                  onClick={() =>
+                    setLayout(item.toLowerCase() as CallLayoutType)
+                  }
+                >
+                  {item}
+                </DropdownMenuItem>
+              </div>
+            ))}
+            <DropdownMenuSeparator className="border-dark-1" />
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
