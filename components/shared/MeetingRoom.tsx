89765dae7ac6162444adda5b1ca2cfb428df5cef
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-14 23:55:57 -0600

    feat(EndCallButton): implement the complete EndCallButton component along the end call for everyone feature

--- components/shared/MeetingRoom.tsx
+++ components/shared/MeetingRoom.tsx
@@ -19,10 +19,14 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { LayoutList, Users } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import EndCallButton from "./EndCallButton";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
+  const searchParams = useSearchParams();
+  const isPersonalRoom = !!searchParams.get("personal");
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
   const [showParticipants, setShowParticipants] = useState(false);
 
@@ -86,6 +90,8 @@ const MeetingRoom = () => {
             <Users size={20} className="text-white" />
           </div>
         </button>
+
+        {!isPersonalRoom && <EndCallButton />}
       </div>
     </section>
   );
