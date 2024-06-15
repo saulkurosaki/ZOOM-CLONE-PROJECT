c79531d470940ab42c28125917e6e9c10993b042
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-14 23:36:04 -0600

    feat(MeetingRoom): implement the participants settings sidebar

--- components/shared/MeetingRoom.tsx
+++ components/shared/MeetingRoom.tsx
@@ -18,7 +18,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LayoutList } from "lucide-react";
+import { LayoutList, Users } from "lucide-react";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
@@ -81,6 +81,11 @@ const MeetingRoom = () => {
           </DropdownMenuContent>
         </DropdownMenu>
         <CallStatsButton />
+        <button onClick={() => setShowParticipants((prev) => !prev)}>
+          <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+            <Users size={20} className="text-white" />
+          </div>
+        </button>
       </div>
     </section>
   );
