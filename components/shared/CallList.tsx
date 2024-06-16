879c32c7aec125866263dfbbf18229bb0a3b4c77
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-15 22:31:11 -0600

    feat(CallList): implement the complete getCalls function

--- components/shared/CallList.tsx
+++ components/shared/CallList.tsx
@@ -1,8 +1,28 @@
+"use client";
+
 import { useGetCalls } from "@/hooks/useGetCalls";
+import { CallRecording } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
   const { endedCalls, upcomingCalls, callRecordings, isLoading } =
     useGetCalls();
+  const router = useRouter();
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+
+  const getCalls = () => {
+    switch (type) {
+      case "ended":
+        return endedCalls;
+      case "recordings":
+        return recordings;
+      case "upcoming":
+        return upcomingCalls;
+      default:
+        return [];
+    }
+  };
 
   return <div>CallList</div>;
 };
