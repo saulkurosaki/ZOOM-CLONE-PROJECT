34ad7b414b392e4092d147392acfd2f1c509a546
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-16 20:52:30 -0600

    fix(CallList): fix little component bug when the previous meeting was a personal meeting

--- components/shared/CallList.tsx
+++ components/shared/CallList.tsx
@@ -86,9 +86,9 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
                 : "/icons/recordings.svg"
             }
             title={
-              (meeting as Call).state?.custom.description.substring(0, 26) ||
-              meeting.filename.substring(0, 20) ||
-              "No description"
+              (meeting as Call).state?.custom?.description?.substring(0, 26) ||
+              meeting?.filename?.substring(0, 20) ||
+              "Personal Meeting"
             }
             date={
               (meeting as Call).state?.startsAt?.toLocaleString() ||
